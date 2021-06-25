396bbc11eb5d1e8630222e94c440b272946b9aaf
chikkuthomas <84303172+chikkuthomas@users.noreply.github.com>
2021-06-25 16:17:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
-// function used to print
-//  python print()
-// js console.log()
-// to run in terminal node filename (.js extension)
+function createBook() {
+  // alert("inside create book")
+  let bookname = boook.value;
+  let author = athor.value;
+  let price = prce.value;
+  let pages = page.value;
+  let book = {
+    bookname, author, price, pages
+  }
+  localStorage.setItem(bookname, JSON.stringify(book))
+  alert("book created")
 
-console.log("hello word")
-
-// location_name,company_name var,let,const, block level we use let
-const pie=3.14
-
-var location_name="kakkanad"
-var company_name="luminar technolab"
-
-for(let i=0;i<10;i++){
-    console.log(i)
 }
 
-// console.log("our company",company_name,"located in ",location_name)
+// to display the book details in a card
+function displayBook(item) { // calling function with argument "item"
+  let htmldata = `<div class="card" style="width: 18rem;"> 
+  <div class="card-header">
+    ${item.bookname} 
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${item.author}</li>
+    <li class="list-group-item">${item.price}</li>
+    <li class="list-group-item">${item.pages}</li>
+  </ul>
+</div>`
+  result.innerHTML = htmldata // to insert "htmldata" to the html page where id="result"
+}
 
-// DATA TYPES
+// to search a book the user input //
+function findBook() {
+  let search = serch.value // value from search box
+  console.log(search);
+  let book = JSON.parse(localStorage.getItem((search))) // check with that bookname as key[bookname]
+  if (book) { // if present in local storage
+    displayBook(book) // call the function display book [book details]
+  }
+  else {
+    console.log("not found");
+  }
 
-var age=15;
-var avg=12.5;
-var name="ram"
-console.log(typeof(age)); // Number
-console.log(typeof(avg)); //Number
-console.log(typeof(name)); //String
+}
 
-// number, string, boolean, undefined,null
+// to get all the books listed in the local storage//
+function allBooks() {
+ 
+  let books = []; //  setting variable "books" as empty array
+  for (let i = 0; i < localStorage.length; i++) { // iterating each elements in the local storage//
+    books.push(JSON.parse(localStorage.getItem(localStorage.key(i)))) 
+  }//pushing to empty array //getting elements   // getting key value with index position
+  
+  console.log(books); // printing details
+  let htmldata = "" // htmldata set as empty
+  for (let item of books) { // iterating each book item & adding to htmldata
+    htmldata += `<div class="card" style="width: 18rem;">
+    <div class="card-header">
+      ${item.bookname}
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${item.author}</li>
+      <li class="list-group-item">${item.price}</li>
+      <li class="list-group-item">${item.pages}</li>
+    </ul>
+  </div>`
 
-// var=>global declaration
-// let=> block level scope
-// const=> constant
+  }
+  result.innerHTML = htmldata // when list all button is clicked htmldata is displayed
+}
 
-// console.log() => to print output
+// to do , user name , task name, completed status - true or false, create and list all button
 
-//flowcontrolls(decision making,looping)
 
-// arithmetic operational (+,-,*,**,/)
-
-// logical operator (&, |,^)
-
-// ++, -- increment,decrement
-
-// for floor division: Math.floor(10/3)
